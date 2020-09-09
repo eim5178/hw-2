a00a2ac0a1e20c03ca773e486b019b7bb2c0e5e6
eim5178 <eim5178@psu.edu>
2020-09-09 13:23:16 +0000

    where a print statement is

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
-def  getGradePoint(course_credit, course_grade,i):
-  course_credit = (course_credit)
+def  getGradePoint(course_grade):
   if course_grade == 'A' :
     gp_course = 4.0
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
   elif course_grade == 'A-' :
     gp_course = 3.67
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
   elif course_grade == 'B+' :
     gp_course = 3.33
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
   elif course_grade == 'B' :
     gp_course = 3.0
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
   elif course_grade == 'B-' :
     gp_course = 2.67
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
   elif course_grade == 'C+':
     gp_course = 2.33
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
   elif course_grade == 'C' :
     gp_course = 2.0
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
   elif course_grade == 'D' :
     gp_course = 1.0
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
   elif course_grade == 'F' :
     gp_course = 0.0
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
   else:
     gp_course = 0.0
-    print (f"Grade point for course {i+1} is: {gp_course}")
     return gp_course
 
 def run():
@@ -47,7 +36,8 @@ def run():
   for i in range (3):
     course_grade = input(f"Enter your course {i+1} letter grade: ")
     course_credit = int(input(f"Enter your course {i+1} credit: "))
-    gp_course = getGradePoint(course_credit, course_grade,i)
+    gp_course = getGradePoint(course_grade)
+    print (f"Grade point for course {i+1} is: {gp_course}")
     calc_num = course_credit * gp_course + calc_num
     total_course_credit = total_course_credit + course_credit
   calc_total = calc_num/total_course_credit

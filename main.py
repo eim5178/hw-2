81ccca38b2718db00fecaa5fa98d019402e3c345
eim5178 <eim5178@psu.edu>
2020-09-09 13:16:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+def  getGradePoint(course_credit, course_grade,i):
+  course_credit = (course_credit)
+  if course_grade == 'A' :
+    gp_course = 4.0
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+  elif course_grade == 'A-' :
+    gp_course = 3.67
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+  elif course_grade == 'B+' :
+    gp_course = 3.33
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+  elif course_grade == 'B' :
+    gp_course = 3.0
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+  elif course_grade == 'B-' :
+    gp_course = 2.67
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+  elif course_grade == 'C+':
+    gp_course = 2.33
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+  elif course_grade == 'C' :
+    gp_course = 2.0
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+  elif course_grade == 'D' :
+    gp_course = 1.0
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+  elif course_grade == 'F' :
+    gp_course = 0.0
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+  else:
+    gp_course = 0.0
+    print (f"Grade point for course {i+1} is: {gp_course}")
+    return gp_course
+
+def run():
+  calc_num = 0
+  total_course_credit =0
+  for i in range (3):
+    course_grade = input(f"Enter your course {i+1} letter grade: ")
+    course_credit = int(input(f"Enter your course {i+1} credit: "))
+    gp_course = getGradePoint(course_credit, course_grade,i)
+    calc_num = course_credit * gp_course + calc_num
+    total_course_credit = total_course_credit + course_credit
+  calc_total = calc_num/total_course_credit
+  print(f"Your GPA is: {calc_total}")
+if __name__ == '__main__':
+  run()
